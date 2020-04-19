348790d13e156009ebc40f96852cdb38f48ad6b7
Zohaib Khan <wrongzoayb@gmail.com>
2020-04-19 21:47:53 +0530

    :lipstick: Create the structure with Native Base

--- App.js
+++ App.js
@@ -19,6 +19,8 @@ import {
   Container,
   Header,
   Content,
+  Card,
+  CardItem,
   Button,
   Text,
   Left,
@@ -26,7 +28,11 @@ import {
   Icon,
   Body,
   Title,
+  H1,
+  H3,
 } from 'native-base';
+
+import {Form, Item, Input, Label} from 'native-base';
 
 const App = () => {
   return (
@@ -47,13 +53,83 @@ const App = () => {
             </Body>
             <Right />
           </Header>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Button full success>
-                <Text>I am a button</Text>
-              </Button>
+          <Content padder>
+            <Card style={{...styles.row, justifyContent:'center'}}>
+              <CardItem style={styles.col4}>
+                <Body>
+                  <Text>Income</Text>
+                  <Text style={styles.highlight}>$900</Text>
+                </Body>
+              </CardItem>
+              <CardItem
+                style={{
+                  ...styles.col4,
+                  borderWidth: 1,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderLeftColor: '#ccc',
+                  borderRightColor: '#ccc',
+                }}>
+                <Body>
+                  <Text>Expense</Text>
+                  <Text style={styles.highlight}>$600</Text>
+                </Body>
+              </CardItem>
+              <CardItem style={styles.col4}>
+                <Body>
+                  <Text>Balance</Text>
+                  <Text style={styles.highlight}>$300</Text>
+                </Body>
+              </CardItem>
+            </Card>
+
+            {/* Transactions */}
+            <View style={styles.section}>
+              <H1 style={styles.sectionTitle}>List of transactions</H1>
+              <Card>
+                <CardItem style={styles.row}>
+                  <Body style={styles.row}>
+                    <Text>Cinema</Text>
+                    <Text>-$30</Text>
+                  </Body>
+                </CardItem>
+              </Card>
+              <Card>
+                <CardItem style={styles.row}>
+                  <Body style={styles.row}>
+                    <Text>Home</Text>
+                    <Text>-$100</Text>
+                  </Body>
+                </CardItem>
+              </Card>
+              <Card>
+                <CardItem style={styles.row}>
+                  <Body style={styles.row}>
+                    <Text>Return</Text>
+                    <Text>$80</Text>
+                  </Body>
+                </CardItem>
+              </Card>
             </View>
-          </View>
+
+            {/* Add Transaction */}
+            <View style={styles.section}>
+              <H1 style={styles.sectionTitle}>Add your transaction</H1>
+              <Form>
+                <Item stackedLabel style={styles.sectionDescription}>
+                  <Label>Text</Label>
+                  <Input />
+                </Item>
+                <Item stackedLabel last style={styles.sectionDescription}>
+                  <Label>Amount</Label>
+                  <Input />
+                </Item>
+                <Button full primary style={styles.section}>
+                  <Text> Add data </Text>
+                </Button>
+              </Form>
+            </View>
+          </Content>
         </ScrollView>
       </SafeAreaView>
     </>
@@ -64,38 +140,48 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: '#eee',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: '#fff',
   },
-  sectionContainer: {
-    marginTop: 62,
-    paddingHorizontal: 24,
+  container: {
+    paddingHorizontal: 16,
+    marginTop: 8,
+  },
+  section: {
+    marginTop: 24,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
     color: '#000',
+    paddingBottom: 8,
+    marginVertical: 16,
+    borderWidth: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
   sectionDescription: {
     marginTop: 8,
+    marginLeft: 0,
+    paddingLeft: 0,
     fontSize: 18,
     fontWeight: '400',
     color: '#000',
   },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 16,
+  },
+  col4: {
+    flex: 1,
+    paddingHorizontal: 4,
+    textAlign: 'center',
+  },
   highlight: {
     fontWeight: '700',
-  },
-  footer: {
-    color: '#000',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    fontSize: 24,
   },
 });
 
