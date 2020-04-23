1c8c8d8bc57892c1943aab24ac21bd1988c310e4
ZowWeb <wrongzoayb@gmail.com>
2020-04-23 13:50:03 +0530

    :art: Improve code structure

--- App.js
+++ App.js
@@ -7,32 +7,14 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
 
-import {
-  Root,
-  Header,
-  Content,
-  Button,
-  Text,
-  Left,
-  Right,
-  Icon,
-  Body,
-  Title,
-  H1,
-  H3,
-} from 'native-base';
+import {Root, Content} from 'native-base';
 
-import {Overview} from './components/Overview';
-import {TransactionList} from './components/TransactionList';
-import {AddTransaction} from './components/AddTransaction';
+import Navbar from './components/Navbar';
+import Overview from './components/Overview';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 import {GlobalProvider} from './context/GlobalState';
 
@@ -44,26 +26,11 @@ const App = () => {
         <SafeAreaView>
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}>
-            <Header>
-              <Left>
-                <Button transparent>
-                  <Icon name="menu" />
-                </Button>
-              </Left>
-              <Body>
-                <Title>Header</Title>
-              </Body>
-              <Right />
-            </Header>
+            style={{backgroundColor: '#eee'}}>
+            <Navbar />
             <Content padder>
-              {/* Overview */}
               <Overview />
-
-              {/* Transactions */}
               <TransactionList />
-
-              {/* Add Transaction */}
               <AddTransaction />
             </Content>
           </ScrollView>
@@ -72,39 +39,5 @@ const App = () => {
     </GlobalProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#eee',
-  },
-  body: {
-    backgroundColor: '#fff',
-  },
-  container: {
-    paddingHorizontal: 16,
-    marginTop: 8,
-  },
-  section: {
-    marginTop: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: '#000',
-    paddingBottom: 8,
-    marginVertical: 16,
-    borderWidth: 0,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    marginLeft: 0,
-    paddingLeft: 0,
-    fontSize: 18,
-    fontWeight: '400',
-    color: '#000',
-  },
-});
 
 export default App;
